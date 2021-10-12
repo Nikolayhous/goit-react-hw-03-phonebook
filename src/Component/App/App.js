f01b844b405e108db43c0c1efb02d4a84149b16d
Николай Гребенюк <Yanukhous@ukr.net>
2021-10-12 12:47:01 +0300

    add localStorage

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -52,6 +52,24 @@ class App extends Component {
         }));
     };
 
+    componentDidMount() {
+        const contacts = localStorage.getItem('contacts');
+        const parsedContacts = JSON.parse(contacts);
+        if (parsedContacts) {
+            this.setState({ contacts: parsedContacts });
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.contacts !== prevState.contacts) {
+            console.log('componentDidUpdate');
+            localStorage.setItem(
+                'contacts',
+                JSON.stringify(this.state.contacts),
+            );
+        }
+    }
+
     render() {
         const { filter } = this.state;
         const { deleteContactList, formSubmitHandler, changeFilter } = this;
